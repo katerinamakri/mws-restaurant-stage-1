0fb075e9dad70088dd8a84715c7222f6bd638c40
katerinamakri <kathrin023@gmail.com>
2018-07-04 00:03:19 +0300

    Last corrections

--- service-worker.js
+++ service-worker.js
@@ -25,23 +25,19 @@ var cacheFiles = [
 
 self.addEventListener('install', function(e) {
     console.log('ServiceWorker Installed');
-
     // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
-
     	// Open the cache
 	    caches.open(cacheName).then(function(cache) {
-
 	    	// Add all the default files to the cache
-			console.log('[ServiceWorker] Caching cacheFiles');
+			console.log('ServiceWorker Caching cacheFiles');
 			return cache.addAll(cacheFiles);
 	    })
-	); // end e.waitUntil
+	);
 });
 
 self.addEventListener('fetch', function(e) {
 	console.log('ServiceWorker Fetch', e.request.url);
-
 	// e.respondWidth Responds to the fetch event
 	e.respondWith(
 		// Check in cache for the request being made
@@ -54,7 +50,7 @@ self.addEventListener('fetch', function(e) {
 				}
 				// If the request is NOT in the cache, fetch
 				return fetch(e.request);
-			}) // end caches.match(e.request)
-	); // end e.respondWith
+			})
+	);
 });
 
